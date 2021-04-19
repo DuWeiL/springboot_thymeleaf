46631b2e6d86c5cfec0c9e2a6b766288aa217d87
duwei <duwei@qq.com>
2021-04-19 21:49:52 +0800

    分支测试02

---
+++ src/main/java/com/offcn/controller/TestController02.java
@@ -0,0 +1,15 @@
+package com.offcn.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: DuWei
+ * @Date: 2021/04/19/21:40
+ * @Description:
+ */
+public class TestController02 {
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
